86f7803daf7c2ae0ac031ff62c677d7b88370a4f
Jared Miller <jcm1317@Jareds-MacBook-Air.local>
2014-10-12 22:00:17 -0700

    added stubs for routes

--- app/routes.js
+++ app/routes.js
@@ -2,13 +2,31 @@
  * Created by jcm1317 on 10/6/14.
  */
 
-module.exports = function(app) {
+module.exports = function(app, router) {
 
     /**
      * Show the homepage
      */
-    app.get('/', function(req, res) {
+    router.get('/', function(req, res) {
         res.render('/var/www/html/index.ejs')
     });
+
+
+    // post data on user. For now we assume the userid is unique and persitant to each user.
+    //
+    router.route('/get_users').post(function(req, res) {
+
+        if (!req.body.hasOwnProperty('userId') ||
+            !req.body.hasOwnProperty('timestamp') ||
+            !req.body.hasOwnProperty('sensorData')) {
+
+            // log bad request
+            res.statusCode = 400;
+            return res.send('Error 400 : missing request data');
+        }
+        //myDb.add_data(req.body.userId, req.body.timestamp, req.body.sensorData);
+    });
+
+    app.use('/api', router);
 
 };
--- server.js
+++ server.js
@@ -6,16 +6,32 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
+var bodyParser = require('body-parser');
+var router = express.Router();
+
 
 var morgan = require('morgan');
 
 //configuration =========================================================
 app.use(morgan('dev')); // for logging requests to the console
 
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// error handling
+app.use(function(err, req, res, next) {
+    //do logging and user-friendly error message display
+    res.send(500);
+})
+
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 //routes ================================================================
-require('./app/routes.js')(app);
+var router = express.Router(); 				// get an instance of the express Router
+
+require('./app/routes.js')(app, router);
 
 
 //launch ================================================================
