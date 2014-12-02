fd65f364689e6e18e465b57fc5257692dc3be351
Nicholas Shahan <nick.shahan@gmail.com>
2014-12-01 17:12:35 -0800

    increased size limit for the reqeust body

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(bodyParser({limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
